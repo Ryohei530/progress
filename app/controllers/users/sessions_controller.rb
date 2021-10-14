05f04bf49ee52049a17607d68d7d753fcd3322e8
Ryohei530 <kuboryo777@gmail.com>
2021-10-14 13:49:41 +0900

    customize regi and session controller, modify views and user model

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,14 +9,23 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    # super
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:success, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    # super
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :success, :signed_out if signed_out
+    yield if block_given?
+    respond_to_on_destroy
+  end
 
   # protected
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_074636) do
+ActiveRecord::Schema.define(version: 2021_10_13_104821) do
 
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
@@ -229,8 +229,6 @@ ActiveRecord::Schema.define(version: 2021_09_27_074636) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
-    t.string "remember_digest"
     t.boolean "admin", default: false
     t.text "bio"
     t.string "encrypted_password", default: "", null: false
