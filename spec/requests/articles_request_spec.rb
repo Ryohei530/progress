18446e15d17ca8b5bf405d7940e2a9fd165c888f
Ryohei530 <kuboryo777@gmail.com>
2022-02-09 15:23:38 +0900

    modify rspec

--- spec/requests/articles_request_spec.rb
+++ spec/requests/articles_request_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Articles", type: :request do
   let(:user) { FactoryBot.create(:user) }
+  let!(:goal) { FactoryBot.create(:goal, user_id: user.id) }
   let!(:article) { FactoryBot.create(:article, user_id: user.id) }
 
   describe "GET /index" do
